c2afdcae5585434a451410cc9c3d6c72e7c89763
Adriano Valle <adrianovalle@mac.com>
2015-03-14 02:17:54 -0400

    made getLocation function and fixed Saturday to Sunday discrepancy

--- weather.rb
+++ weather.rb
@@ -1,49 +1,52 @@
 require 'yahoo_weatherman'
 
-puts "What is your location?  Enter city or zipcode"
-location = gets.chomp
+
+def getLocation
+  puts "What is your location?  Enter city or zipcode"
+  location = gets.chomp
+
+  client = Weatherman::Client.new
+  client.lookup_by_location(location)
+end
 
 def getCurrentWeather(location)
 
-  client = Weatherman::Client.new
-
-  tempInC = client.lookup_by_location(location).condition['temp']
+  tempInC = location.condition['temp']
   tempInF = tempInC * 9 / 5 + 32
-  forecast = client.lookup_by_location(location).condition['text']
+  forecast = location.condition['text'].downcase
 
   puts "It is currently #{tempInF}°F and #{forecast}."
 
 end
 
-def getForecast(location)
-  client = Weatherman::Client.new
-  weatherman = client.lookup_by_location(location)
-  forecasts = weatherman.forecasts
+def getForecast(location, today)
+  forecasts = location.forecasts
 
   forecasts.each do |forecast|
-    date = forecast["date"]
-    day = DateTime.iso8601("#{date}").strftime('%A')
-    weather = forecast["text"]
+    weekday = forecast["date"]
+    day = weekday.strftime('%A')
+    weather = forecast["text"].downcase
     high = forecast["high"].to_i * 9 / 5 + 32
     low = forecast["low"].to_i * 9 / 5 + 32
 
-    d = Time.now
-    today = d.strftime('%w')
-    dayNum = DateTime.iso8601("#{date}").strftime('%w')
+    dayNum = weekday.strftime('%w').to_i
 
     if dayNum == today
-      day = "Today"
-    elsif dayNum.to_i == today.to_i + 1
-      day = "Tomorrow"
+      weekday = "Today"
+    elsif dayNum == today + 1 || dayNum == today - 6
+      weekday = "Tomorrow"
     else
-      day
+      weekday = day
     end
 
-    puts "#{day} the forecast is #{weather}, with a high of #{high}˚F and a low of #{low}˚F."
+    puts "#{weekday} the forecast is #{weather}, with a high of #{high}˚F and a low of #{low}˚F."
   end
 end
 
+location = getLocation
+today = Time.now.strftime('%w').to_i
+
 getCurrentWeather(location)
-getForecast(location)
+getForecast(location, today)
 
 
